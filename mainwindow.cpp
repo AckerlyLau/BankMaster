53e76c1d0cd812770bfebc1695710b4783816e12
SilentChildhood <32513057+SilentChildhood@users.noreply.github.com>
2019-10-24 21:39:18 +0800

    出BUG了，mainwiondow不能自动生成代码

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 #include "System.h"
 #include "CreateAccountDialog.h"
 #include "RegisterDialog.h"
+#include "QListWidget"
+
 MainWindow::MainWindow(System *oss ,QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -28,3 +30,5 @@ void MainWindow::on_actionRegister_R_triggered()
     RegisterDialog rsg(os);
     rsg.exec();
 }
+
+
